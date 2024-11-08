e5c825c9fd86292502ce37074f5509ce1b80bd0e
好想生吃派蒙 <h880019@163.com>
2024-11-08 23:54:14 +0800

    Delete src/assets/assets/icon.py

--- src/assets/assets/icon.py
+++
@@ -1,9 +0,0 @@
-from os.path import join
-from pygame.image import load
-from src.assets.assets.images.path import images_path
-
-#path
-icon_path = join(images_path, 'icons')
-
-#icon
-icon = load(join(icon_path, 'icon.png'))
